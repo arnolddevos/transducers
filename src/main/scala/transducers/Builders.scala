ee360e620a0687ac68e345785862fd39b66dd747
Arnold deVos <adv@langdale.com.au>
2016-05-01 17:41:58 +1000

    a string builder

--- src/main/scala/transducers/Builders.scala
+++ src/main/scala/transducers/Builders.scala
@@ -1,4 +1,4 @@
-package transducers 
+package transducers
 
 import collection.mutable.{Builder, ListBuffer, ArrayBuilder}
 import collection.immutable.VectorBuilder
@@ -6,10 +6,29 @@ import reflect.ClassTag
 
 trait Builders { this: Transducers =>
 
+  def buildString[X](s1: String="", s2: String="", s3: String=""): Reducer[X, String] = new Reducer[X, String] {
+    class State {
+      var builder = new java.lang.StringBuilder(s1)
+      var count = 0
+    }
+    def init = new State
+    def apply(s: State, x: X) = inContext {
+      if(s.count > 0) s.builder append s2
+      s.builder append x.toString
+      s.count += 1
+      s
+    }
+    def isReduced(s: State) = false
+    def complete(s: State) = {
+      s.builder append s3
+      s.builder.toString
+    }
+  }
+
   def builder[X, S](br: => Builder[X, S]): Reducer[X, S] = new Reducer[X, S] {
     type State = Builder[X, S]
     def init = br
-    def apply(xs: State, x: X) = inContext { xs += x; xs }
+    def apply(xs: State, x: X) = inContext { xs += x }
     def isReduced(xs: State) = false
     def complete(xs: State) = xs.result
   }
