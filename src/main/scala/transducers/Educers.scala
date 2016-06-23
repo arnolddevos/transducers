6286daa6118b7a3bb5a0f706ba56f93b9c547490
Arnold deVos <adv@langdale.com.au>
2016-06-24 09:34:30 +1000

    spans transducer

--- src/main/scala/transducers/Educers.scala
+++ src/main/scala/transducers/Educers.scala
@@ -66,6 +66,20 @@ trait Educers { this: Transducers =>
     }
   }
 
+  def spans[A, S]( f: Reducer[A, S]): Transducer[S, A] = new Transducer[S, A] {
+    def apply[T]( h: Reducer[S, T]): Reducer[A, T] = new Reducer[A, T] {
+      type State = (f.State, h.State)
+      def init = (f.init, h.init)
+      @annotation.tailrec
+      def apply(s: State, a: A): State = {
+        if(f.isReduced(s._1)) apply((f.init, h(s._2, f.complete(s._1))), a)
+        else (f(s._1, a), s._2)
+      }
+      def isReduced(s: State) =  h.isReduced(s._2)
+      def complete(s: State) = h.complete(s._2)
+    }
+  }
+
   def transducer[A, B, T](t0: T)(f: (T, A) => (T, B)): Transducer[B, A] = new Transducer[B, A] {
     def apply[S](r: Reducer[B, S]) = new Reducer[A, S] {
       type State = (r.State, T)
