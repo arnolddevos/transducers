ead7914973a1edc6ba9e197431829a1bf96f2d2f
Arnold deVos <adv@langdale.com.au>
2016-09-08 13:00:40 +1000

    use the stateful reductions for efficiency

--- src/main/scala/transducers/package.scala
+++ src/main/scala/transducers/package.scala
@@ -2,7 +2,7 @@ package object transducers
   extends Transducers
   with Views
   with Operators
-  with ImmutableStateOperators
+  with StatefulOperators
   with Educers
   with Builders
   with Aggregates
