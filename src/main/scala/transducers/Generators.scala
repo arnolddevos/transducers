a32e613d1c7fefeaf01af28f74b9666c86bee28d
Arnold deVos <adv@langdale.com.au>
2016-12-19 12:00:33 +1100

    Series is almost a comonad

--- src/main/scala/transducers/Generators.scala
+++ src/main/scala/transducers/Generators.scala
@@ -136,22 +136,37 @@ trait SeriesDefs { this: Transducers with ContextIsFunctor =>
 
     trait Ops[+A] { this: Series[A] =>
 
+      def map[B](f: A => B): Series[B] = this match {
+        case NonEmpty(a, cs1) => NonEmpty(f(a), cs1 map (_ map f))
+        case Empty => Empty
+      }
+
+      def extract: Option[A] = this match {
+        case NonEmpty(a, _) => Some(a)
+        case Empty => None
+      }
+
+      def extend[B](f: Series[A] => B): Series[B] =this match {
+        case NonEmpty(a, cs1) => NonEmpty(f(this), cs1 map (_ extend f))
+        case Empty => Empty
+      }
+
+
       def +:[B >: A](b: B): Series[B] = NonEmpty(b, inContext(this))
 
       def append[B >: A](s: Series[B]): Series[B] = this match {
         case NonEmpty(a, cs1) => NonEmpty(a, cs1 map (_ append s))
         case Empty => s
       }
-
-      def map[B](f: A => B): Series[B] = this match {
-        case NonEmpty(a, cs1) => NonEmpty(f(a), cs1 map (_ map f))
-        case Empty => Empty
-      }
     }
 
     def apply(): Series[Nothing] = Empty
     def apply[A](a: A, cs: Context[Series[A]]): Series[A] = NonEmpty(a, cs)
     def singleton[A](a: A): Series[A] = NonEmpty(a, inContext(Empty))
+    def fromOption[A](o: Option[A]) = o match {
+      case Some(a) => singleton(a)
+      case None => Empty
+    }
     def fromList[A](as: List[A]): Series[A] = as match {
       case a :: as1 => NonEmpty(a, lazily(fromList(as1)))
       case Nil => Empty
