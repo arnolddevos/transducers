2cb78d6ef44450c71cefd2563bccc57b9eb2fbfb
Arnold deVos <adv@langdale.com.au>
2017-03-18 16:17:02 +1100

    put back some of the Producer operations

--- src/main/scala/transducers/lifted/Induction.scala
+++ src/main/scala/transducers/lifted/Induction.scala
@@ -5,6 +5,62 @@ trait Induction { this: Transducers with ContextIsMonad =>
 
   type Producer[+A] = Context[Series[A]]
 
+  object Producer {
+    import Series._
+
+    def apply(): Producer[Nothing] = inContext(Series())
+    def apply[A](a: A): Producer[A] = inContext(Series(a))
+    def apply[A](a: A, g: Producer[A]): Producer[A] = inContext(Series(a, g))
+    def fromList[A](as: List[A]): Producer[A] = inContext(Series.fromList(as))
+
+    def split[A, B](g: Producer[A])(f: (A, Producer[A]) => Producer[B]): Producer[B] = {
+      g >>= {
+        _ match {
+          case NonEmpty(a, g1) => f(a, g1)
+          case Empty => Producer()
+        }
+      }
+    }
+
+    def map[A, B](g: Producer[A])(f: A => B): Producer[B] = {
+      g map {
+        _ match {
+          case NonEmpty(a, g1) => NonEmpty(f(a), map(g1)(f))
+          case Empty => Empty
+        }
+      }
+    }
+
+    def flatMap[A, B](g: Producer[A])(f: A => Producer[B]): Producer[B] = {
+      g >>= {
+        _ match {
+          case NonEmpty(a, g1) => concat(f(a), flatMap(g1)(f))
+          case Empty => Producer()
+        }
+      }
+    }
+
+    def filter[A](g: Producer[A])(f: A => Boolean): Producer[A] = {
+      g >>= {
+        _ match {
+          case NonEmpty(a, g1) =>
+            val g2 = filter(g1)(f)
+            if(f(a)) Producer(a, g2) else g2
+          case Empty => Producer()
+        }
+      }
+    }
+
+    def concat[A](ga: Producer[A], gb: Producer[A]): Producer[A] = {
+      ga >>= {
+        _ match {
+          case NonEmpty(a, ga1) => Producer(a, concat(ga1, gb))
+          case Empty => gb
+        }
+      }
+    }
+  }
+
   sealed trait Series[+A] extends Series.Ops[A]
 
   object Series {
@@ -40,10 +96,10 @@ trait Induction { this: Transducers with ContextIsMonad =>
 
     def apply(): Series[Nothing] = Empty
     def apply[A](a: A, cs: Context[Series[A]]): Series[A] = NonEmpty(a, cs)
-    def singleton[A](a: A): Series[A] = NonEmpty(a, inContext(Empty))
+    def apply[A](a: A): Series[A] = NonEmpty(a, inContext(Empty))
     def fromOption[A](o: Option[A]) = o match {
-      case Some(a) => singleton(a)
-      case None => Empty
+      case Some(a) => apply(a)
+      case None => apply()
     }
     def fromList[A](as: List[A]): Series[A] = as match {
       case a :: as1 => NonEmpty(a, lazyContext(fromList(as1)))
